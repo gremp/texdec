ed9a5e388c42f6dc030210658159a1ca84c1e749
gremp <me@gremp.gr>
2021-05-08 18:08:09 +0300

    testing new lib

---
+++ scripts/npm-publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+projectFolder=`pwd`
+tmpFolder="/tmp/RELEASE_$RANDOM"
+mkdir $tmpFolder
+npm run tsc
+cd $tmpFolder
+npm pack $projectFolder
+tarfile=`ls`
+tar xvf $tarfile
+cd package
+mv README.md dist
+mv package.json dist
+cd dist
+npm publish
+rm -rf tmpFolder
+cd $projectFolder
+
